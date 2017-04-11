bf4e4598d2ae1587a8115d10764fe10dfe42cda3
fernard <pawel.kubis1989@gmail.com>
2017-04-11 10:40:38 +0200

    Converted to OLOO

--- js/app.js
+++ js/app.js
@@ -1,151 +1,153 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    const GameOfLife = {
 
-    function GameOfLife(boardWidth, boardHeight) {
+        init(boardWidth, boardHeight) {
 
-        this.height = boardHeight;
-        this.width = boardWidth;
-        this.board = document.querySelector('#board');
-        this.cells = [];
-        this.boardState = [];
-
-
-    }
-    GameOfLife.prototype.start = function() {
-
-        this.createBoard();
-        this.firstGlider();
+            this.height = boardHeight;
+            this.width = boardWidth;
+            this.board = document.querySelector('#board');
+            this.cells = [];
+            this.boardState = [];
 
 
-    }
+        },
 
-    GameOfLife.prototype.createBoard = function() {
+        run() {
 
-        this.board.style.width = this.width * 10 + 'px';
-        this.board.style.height = this.height * 10 + 'px';
-        this.numberOfCells = this.width * this.height;
+            this.createBoard();
+            this.firstGlider();
+        },
 
-        for (i = 0; i < this.numberOfCells; i++) {
-            var div = '<div></div>'
-            this.board.innerHTML += div;
+        createBoard() {
 
-        }
-        this.cells = Array.prototype.slice.call(document.querySelectorAll('#board div'));
+            this.board.style.width = this.width * 10 + 'px';
+            this.board.style.height = this.height * 10 + 'px';
+            this.numberOfCells = this.width * this.height;
 
-        this.cells.forEach(function(cell, index) {
+            for (let i = 0; i < this.numberOfCells; i++) {
+                const div = '<div></div>'
+                this.board.innerHTML += div;
 
-            cell.addEventListener('click', function() {
+            }
+            this.cells = Array.prototype.slice.call(document.querySelectorAll('#board div'));
 
-                if (this.className === 'live') {
+            this.cells.forEach(function(cell, index) {
 
-                    this.classList.remove('live');
-                } else {
+                cell.addEventListener('click', function() {
 
-                    this.classList.add('live');
+                    if (this.className === 'live') {
 
+                        this.classList.remove('live');
+                    } else {
+
+                        this.classList.add('live');
+
+                    }
+
+
+                });
+            });
+
+        },
+
+        getCell(x, y) {
+
+            const index = (x - 1) + ((y - 1) * this.width);
+
+            return this.cells[index];
+        },
+
+        cellSetState(x, y, state) {
+
+
+            if (this.getCell(x, y).className === state) {
+                this.getCell(x, y).classList.remove(state);
+
+            } else {
+
+                this.getCell(x, y).classList.add(state);
+
+            }
+
+        },
+
+        firstGlider() {
+
+            this.cellSetState(3, 1, 'live');
+            this.cellSetState(4, 6, 'live');
+            this.cellSetState(1, 11, 'live');
+            this.cellSetState(12, 3, 'live');
+            this.cellSetState(4, 5, 'live');
+
+        },
+
+        computeCellNextState(x, y) {
+
+
+            let livingCells = 0;
+
+            var neighbours = [this.getCell(x - 1, y - 1), this.getCell(x, y - 1), this.getCell(x + 1, y - 1), this.getCell(x - 1, y), this.getCell(x + 1, y), this.getCell(x - 1, y + 1), this.getCell(x, y + 1), this.getCell(x + 1, y + 1)];
+
+            for (let i = 0; i < neighbours.length; i++) {
+
+                if (neighbours[i] !== undefined && neighbours[i].className === "live") {
+
+                    livingCells++;
+                }
+
+            }
+
+
+            if (livingCells === 2 || livingCells === 3) {
+                return 1;
+            }
+
+            return 0;
+        },
+
+        computeNextGeneration() {
+
+            this.boardState = [];
+            for (let i = 1; i <= this.height; i++) {
+
+                for (let j = 1; j <= this.width; j++) {
+
+                    this.boardState.push(this.computeCellNextState(j, i));
                 }
 
 
-            });
-        });
-    };
-
-    GameOfLife.prototype.getCell = function(x, y) {
-
-        var index = (x - 1) + ((y - 1) * this.width);
-
-        return this.cells[index];
-    }
-
-    GameOfLife.prototype.cellSetState = function(x, y, state) {
-        if (this.getCell(x, y).className === state) {
-            this.getCell(x, y).classList.remove(state);
-
-        } else {
-
-            this.getCell(x, y).classList.add(state);
-
-        }
-
-
-    }
-
-    GameOfLife.prototype.firstGlider = function() {
-
-        this.cellSetState(3, 1, 'live');
-        this.cellSetState(4, 6, 'live');
-        this.cellSetState(1, 11, 'live');
-        this.cellSetState(12, 3, 'live');
-        this.cellSetState(4, 5, 'live');
-
-    }
-
-    GameOfLife.prototype.computeCellNextState = function(x, y) {
-
-        var livingCells = 0;
-
-        var neighbours = [this.getCell(x - 1, y - 1), this.getCell(x, y - 1), this.getCell(x + 1, y - 1), this.getCell(x - 1, y), this.getCell(x + 1, y), this.getCell(x - 1, y + 1), this.getCell(x, y + 1), this.getCell(x + 1, y + 1)];
-
-        for (var i = 0; i < neighbours.length; i++) {
-
-            if (neighbours[i] !== undefined && neighbours[i].className === "live") {
-
-                livingCells++;
             }
-
-        }
-
-
-        if (livingCells === 2 || livingCells === 3) {
-            return 1;
-        }
-
-        return 0;
+        },
 
 
+        printNextGeneration() {
 
-    }
+            for (var i = 0; i < this.boardState.length; i++) {
 
-    GameOfLife.prototype.computeNextGeneration = function() {
-        this.boardState = [];
-        for (var i = 1; i <= this.height; i++) {
+                if (this.boardState[i] === 1 && this.cells[i].className !== 'live') {
 
-            for (var j = 1; j <= this.width; j++) {
+                    this.cells[i].classList.add('live');
 
-                this.boardState.push(this.computeCellNextState(j, i));
+
+                } else if (this.boardState[i] === 0 && this.cells[i].className === 'live')
+                    this.cells[i].classList.remove('live');
             }
+        },
+
+        nextGenFn() {
+
+            this.computeNextGeneration();
+            this.printNextGeneration();
+        },
+
+        runInterval(fn) {
 
 
+            this.myInterval = setInterval(fn.bind(this), 500);
         }
 
-    }
 
-    GameOfLife.prototype.printNextGeneration = function() {
-
-        for (var i = 0; i < this.boardState.length; i++) {
-
-            if (this.boardState[i] === 1 && this.cells[i].className !== 'live') {
-
-                this.cells[i].classList.add('live');
-
-
-            } else if (this.boardState[i] === 0 && this.cells[i].className === 'live')
-                this.cells[i].classList.remove('live');
-        }
-
-    }
-
-    GameOfLife.prototype.nextGenFn = function() {
-
-        this.computeNextGeneration();
-        this.printNextGeneration();
-
-    }
-
-    GameOfLife.prototype.runInterval = function() {
-
-        this.nextGenFn = this.nextGenFn.bind(this);
-        setInterval(this.nextGenFn, 500);
 
     }
 
@@ -153,21 +155,25 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#play').addEventListener('click', function(e) {
 
         e.preventDefault();
+        const width = document.querySelector('#board-width').value;
+        const height = document.querySelector('#board-height').value;
+        game.init(width, height);
+        game.run();
+        game.runInterval(game.nextGenFn);
 
-        game.runInterval();
 
     });
 
     document.querySelector('#pause').addEventListener('click', function(e) {
 
         e.preventDefault();
-        clearInterval(game.runInterval);
+        clearInterval(game.myInterval);
 
     });
 
 
-    var game = new GameOfLife(15, 15);
-    game.start();
+    const game = Object.create(GameOfLife);
+
 
 
 
